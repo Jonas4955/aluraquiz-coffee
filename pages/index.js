7a102e16d8736e6e15dfe1e83cc1cb69dede6630
Jonas Antunes <jonas.antunes@ambevtech.com.br>
2021-01-28 22:29:13 -0300

    feat: add router next - create page quiz

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
@@ -18,15 +22,37 @@ const QuizContainer = styled.div`
 `
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = useState('')
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - CSS Awesome</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <form
+              onSubmit={event => {
+                event.preventDefault()
+                router.push(`/quiz?name=${name}`)
+              }}
+            >
+              <input
+                placeholder="Diz aí seu nome"
+                onChange={event => {
+                  setName(event.target.value)
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
